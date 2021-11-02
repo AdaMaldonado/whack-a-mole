1c394bbf9d2c070ec4d206908a2400998c379c5a
Mohammed J. Hossain <jahedev@gmail.com>
2021-11-02 16:38:04 -0400

    html / css / js base files

---
+++ script.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
+canvas.width = 640
+canvas.height = 480
+
+ctx.fillStyle = 'brown'
+ctx.fillRect(0, 0, 640, 480)
